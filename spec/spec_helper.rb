155e208b02f9b0a8452f78cf2fd801fc1f37a4f1
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-05 15:57:07 +0900

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,27 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'bokete_api_wrapper'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+require 'factory_girl'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
-  
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+  config.include FactoryGirl::Syntax::Methods
+  config.before { example.example_group.let(:mock_user) { build(:user) } }
+
+  FactoryGirl.find_definitions
+
+  Niconico.setup do |conf|
+    conf.site_id = :site
+    conf.salt = :salt
+    conf.dev = true
+  end
+
+  def mock_file(file)
+    File.read File.join(File.expand_path('../support', __FILE__), file)
+  end
 end
+
