75a1eb357992c8b53794d5c39348af92a90e5c23
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-06 00:13:20 +0900

    other page

--- lib/bokete_api_wrapper/api/popular.rb
+++ lib/bokete_api_wrapper/api/popular.rb
@@ -7,12 +7,11 @@ module BoketeApiWrapper
 
       def get(page = 0,options = {})
         params = {
-          page: page
+          :page => page
         }
         response = client.get '/popular', params
         doc = Nokogiri::HTML(response)
         raise Error, "status code #{response.code}" unless response.code.to_i == 200
-   
         doc.xpath('//div/input').map{|a|
           a['value']
         }.reject{|a|
@@ -20,23 +19,6 @@ module BoketeApiWrapper
         }.select{|a|
           a.include?('ss.bokete')
         }.uniq
-
-
-        #raise exception_mapping(xml.css("code").text) unless xml.xpath("//@status").text == "ok"
-      end
-
-      private
-      def exception_mapping(code)
-        case code
-        when "INVALIDVIDEO", "PARAMERROR", "EXIST"
-          RestClient::BadRequest
-        when "NONEXIST"
-          RestClient::ResourceNotFound
-        when "INTERNALERROR"
-          RestClient::InternalServerError
-        when "MAINTENANCE"
-          RestClient::ServiceUnavailable
-        end
       end
     end
   end
--- spec/bokete_api_wrapper/api/popular_spec.rb
+++ spec/bokete_api_wrapper/api/popular_spec.rb
@@ -6,7 +6,12 @@ describe BoketeApiWrapper::Api::Popular do
     it 'call api response' do
       stub_request(:get, "http://bokete.jp/popular?page=0").
          to_return(:status => 200, :body => mock_file("api/popular/success.html"), :headers => {})
-      api.get().should have(10).items
+      api.get(0).should have(10).items
+    end
+    it 'other pages' do
+      stub_request(:get, "http://bokete.jp/popular?page=1").
+         to_return(:status => 200, :body => mock_file("api/popular/success.html"), :headers => {})
+      api.get(1).should have(10).items
     end
   end
 end
